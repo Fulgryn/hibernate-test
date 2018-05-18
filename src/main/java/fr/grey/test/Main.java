107454576149b015b001294702f0048ae1e0f698
g_rey <gabdu74@gmail.com>
2018-05-18 11:15:51 +0200

    ajout logs

---
+++ src/main/java/fr/grey/test/Main.java
@@ -0,0 +1,9 @@
+package fr.grey.test;
+
+import fr.grey.dao.DataBaseHelper;
+
+public class Main {
+    public static void main(String[] args) {
+        DataBaseHelper.createEntityManager();
+    }
+}
